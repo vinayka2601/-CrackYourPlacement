f46e20ff9fd724c1e675f2adfce9e7b6ecef31cc
vinayka2601 <vinaykaawasthi26@gmail.com>
2024-08-06 19:01:46 +0530

    fact

--- factors.java
+++ factors.java
@@ -1,10 +1,16 @@
 public class factors {
     public static void main(String[] args) {
-        int n=20;
+        int n=6;
+        int sum=0;
         for(int i=1;i<=n;i++){
+           
             if(n%i==0){
                 System.out.println(i);
+                sum=sum+i;
             }
+           
+            
+       System.out.println("sum is"+sum);
            
         }
     }
